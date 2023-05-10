0996d2f7a74aa26d0397eceae8a339bee337bd35
ivanglie <ivanglie22@gmail.com>
2023-05-10 18:33:45 +0300

    Add some tests

--- cbr_test.go
+++ cbr_test.go
@@ -1,25 +1,57 @@
 package cbr
 
 import (
+	"errors"
+	"net/http"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_Debug(t *testing.T) {
-	Debug = true
-	rate("CNY", time.Now())
-	assert.True(t, Debug)
+type MockHttpClient struct{}
 
-	Debug = false
-	rate("CNY", time.Now())
-	assert.False(t, Debug)
+func (m *MockHttpClient) Do(req *http.Request) (*http.Response, error) {
+	return &http.Response{StatusCode: 500}, nil
 }
 
-func Test_rate_Error(t *testing.T) {
-	rate, err := rate("", time.Now())
-	assert.NotNil(t, err)
+type MockHttpClientErr struct{}
+
+func (m *MockHttpClientErr) Do(req *http.Request) (*http.Response, error) {
+	return nil, errors.New("error")
+}
+
+func TestClient_GetRate(t *testing.T) {
+	Debug = true
+
+	client := NewClient()
+	rate, err := client.GetRate("USD", time.Now())
+	assert.Nil(t, err)
+	assert.GreaterOrEqual(t, rate, float64(1))
+}
+
+func TestClient_GetRate_Error(t *testing.T) {
+	Debug = false
+
+	// unknown currency: _
+	client := NewClient()
+	rate, err := client.GetRate("_", time.Now())
+	assert.Error(t, err)
+	assert.Equal(t, "unknown currency: _", err.Error())
+	assert.Equal(t, float64(0), rate)
+
+	// status code: 500
+	client.httpClient = &MockHttpClient{}
+	rate, err = client.GetRate("CNY", time.Now())
+	assert.Error(t, err)
+	assert.Equal(t, "status code: 500", err.Error())
+	assert.Equal(t, float64(0), rate)
+
+	// error
+	client.httpClient = &MockHttpClientErr{}
+	rate, err = client.GetRate("CNY", time.Now())
+	assert.Error(t, err)
+	assert.Equal(t, "error", err.Error())
 	assert.Equal(t, float64(0), rate)
 }
 
